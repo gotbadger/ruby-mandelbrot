7e86d7b6a3ca74f721b4ef125cea344488d5297c
Philip Hayton <phil@freeagent.com>
2016-06-17 14:47:56 +0100

    refactor main to use libs

--- main.rb
+++ main.rb
@@ -1,18 +1,7 @@
-require "complex"
 require "colorize"
+require_relative "lib/mandelbrot"
 
 MAX_ITERATIONS = String.colors.count
-
-def mandelbrot(a)
-  base_case = [a, 0]
-  Array.new(MAX_ITERATIONS, base_case).inject(base_case) do |prev ,base|
-    z, itr = prev
-    c, _ = base
-    val = z*z + c
-    itr += 1 unless val.abs < 2
-    [val, itr]
-  end
-end
 
 def colour(str, itr)
   decode = String.colors[itr]
@@ -20,9 +9,10 @@ def colour(str, itr)
 end
 
 def grid
+  mandelbrot = Mandelbrot.new(MAX_ITERATIONS)
   (1.0).step(-1,-0.02) do |y|
     (-2.0).step(0.5,0.015) do |x|
-      _, itr = mandelbrot(Complex(x,y))
+      _, itr = mandelbrot.calculate(x,y)
       print itr < MAX_ITERATIONS ? "#{colour("@",itr)}" : ' '
     end
     puts
